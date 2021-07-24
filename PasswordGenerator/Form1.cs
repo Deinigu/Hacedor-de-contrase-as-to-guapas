013ced1c9cb0a4941aa17f0e67b56b3e7bf32f7c
BuildTools <unconfigured@null.spigotmc.org>
2021-07-24 23:53:18 +0200

    Agregar archivos de proyecto.

---
+++ PasswordGenerator/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PasswordGenerator
+{
+    public partial class Form1 : Form
+    {
+        private Generador generador = new Generador();
+        public Form1()
+        {
+            InitializeComponent();
+            this.CenterToScreen();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        { 
+                String contrasenya = generador.generarcontrasenya();
+                textBox1.Text = contrasenya;
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            bool prueba = int.TryParse(textBox2.Text, out int result);
+            if(prueba)
+            {
+                generador.setNumCaracteres(int.Parse(textBox2.Text));
+            }
+            
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("EASTER EGG :)");
+        }
+    }
+}
---
+++ PasswordGenerator/Generador.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasswordGenerator
+{
+    class Generador
+    {
+        private char[] VOCABULARIO = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','ñ','o','p','q','r','s','t','u','v','w','x','y','z',
+                                      'A','B','C','D','E','F','G','H','I','J','K','L','M','N','Ñ','O','P','Q','R','S','T','U','V','W','X','Y','Z',
+                                      '@','#','|','~','€','$'};
+        private int nCaracteres = 10;
+        private String contrasenya;
+        public Generador()
+        {
+            this.contrasenya = "No";
+        }
+
+        public String generarcontrasenya()
+        {
+            Random generadorAleatorio = new Random();
+            String res = VOCABULARIO[generadorAleatorio.Next(VOCABULARIO.Length)].ToString() ;
+            for (int i = 0; i < nCaracteres-1; i++)
+            {
+                res += VOCABULARIO[generadorAleatorio.Next(VOCABULARIO.Length)];
+            }
+            this.contrasenya = res;
+            return this.contrasenya;
+        }
+
+        public void setNumCaracteres(int nCaracteres)
+        {
+            this.nCaracteres = nCaracteres;
+        }
+    }
+}
